5cc41fc5f01a93f4266e81a7541e379bfd3cc1ab
White0027 <116343286+White0027@users.noreply.github.com>
2023-11-07 09:22:25 +0100

    Add files via upload

---
+++ Strom/Strom/Strom.cpp
@@ -0,0 +1,39 @@
+﻿// Strom.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+
+void Koruna(int vyska) {
+    for (int i = 0; i < vyska; ++i) {
+        for (int j = 0; j < vyska - i; ++j) {
+            std::cout << " ";
+        }
+        for (int k = 0; k <= i; k++) {
+            std::cout << " *";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Kmen(int vyska, int sirka) {
+    for (int i = 0; i < vyska; ++i) {
+        for (int j = 0; j < sirka; ++j)
+            std::cout << " ";
+        std::cout << "##" << std::endl;
+    }
+}
+
+int main() {
+    int koruna, kmen;
+
+    std::cout << "Vyska koruny: ";
+    std::cin >> koruna;
+ 
+    std::cout << "Vyska kmene: ";
+    std::cin >> kmen;
+
+    Koruna(koruna);
+    Kmen(kmen, koruna);
+
+    return 0;
+}
