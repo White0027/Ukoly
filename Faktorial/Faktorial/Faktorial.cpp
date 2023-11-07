98f10fd9ad851227c38a8034ce484b5dc6b56bf9
White0027 <116343286+White0027@users.noreply.github.com>
2023-11-07 09:31:17 +0100

    Add files via upload

---
+++ Faktorial/Faktorial/Faktorial.cpp
@@ -0,0 +1,30 @@
+﻿// Faktorial.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+
+int faktorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return n * faktorial(n - 1); 
+    }
+}
+
+int main() {
+    int cislo;
+
+    std::cout << "Zadej cislo: ";
+    std::cin >> cislo;
+
+    if (cislo < 0) {
+        std::cout << "Faktorial nelze spocitat pro zaporna cisla." << std::endl;
+    }
+    else {
+        int vysledek = faktorial(cislo);
+        std::cout << "Faktorial " << cislo << " je " << vysledek << std::endl;
+    }
+
+    return 0;
+}
