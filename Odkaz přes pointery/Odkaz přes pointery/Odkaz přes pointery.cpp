76f7839adc8998798d39c505ac8e3436444f0912
White 002 <116343286+White0027@users.noreply.github.com>
2024-01-09 10:02:27 +0100

    Add files via upload

---
+++ Odkaz přes pointery/Odkaz přes pointery/Odkaz přes pointery.cpp
@@ -0,0 +1,62 @@
+﻿// Odkaz přes pointery.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	int val;
+	Node* next;
+};
+
+void printNode(Node* node);
+Node* newNode(int val);
+void addNode(Node* node, int val);
+
+int main()
+{
+	Node* node1 = newNode(1);
+	printNode(node1);
+	
+	Node* node2 = newNode(2);
+	printNode(node2);
+	node1->next = node2;
+	printNode(node1);
+	
+	Node* node3 = newNode(3);
+	printNode(node3);
+	node2->next = node3;
+
+	return 0;
+}
+
+void printNode(Node* node)
+{
+	cout << "[Add: " << node << ", val: " << node->val << ", next: " << node->next << "]" << endl;
+	if (node->next != NULL) printNode(node->next);
+}
+
+Node* newNode(int val)
+{
+	Node* newNode = new Node();
+	newNode->val = val;
+	newNode->next = NULL;
+	return newNode;
+}
+
+void addNode(Node* previous, int val)
+{
+	Node* newNode = new Node();
+	newNode->val;
+	
+	newNode->next = previous->next;
+	previous->next = newNode;
+}
+
+
+
+
+
+
+
